7f794db4cc12110fd8c329887d6d2e171b51eff0
Al <dandavisjs@protonmail.com>
2022-12-08 12:01:14 -0800

    Minor changes to meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,11 @@ class MyDocument extends Document {
             <Html lang="zxx">
                 <Head>
                     <link rel="icon" href="/images/favicon.ico"></link>
+										<meta
+											name="description"
+											content="CDL Help это уникальное приложение с переводом CDL на русский в режиме реального времени. Лучший способ подготовки к сдачи тестов и обучению языка."
+											key="desc"
+										/>
                 </Head>
                 <body>
                     <Main />
