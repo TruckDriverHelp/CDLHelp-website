aeb7feb02b44b808de47c21e6b998ee15b864534
alur2191 <dandavisjs@protonmail.com>
2023-03-25 09:17:03 -0700

    Google tag manager added

--- pages/_document.js
+++ pages/_document.js
@@ -6,13 +6,23 @@ class MyDocument extends Document {
         return (
             <Html lang="zxx">
                 <Head>
-                <Script id="google-tag-manager" strategy="afterInteractive">
-                {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-KQW58WK');`}
-                </Script>
+                    <Script id="google-tag-manager" strategy="afterInteractive">
+                    {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-KQW58WK');`}
+                    </Script>
+                    <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-11129277199">
+                    </Script>
+                    <Script>
+                        {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+
+                        gtag('config', 'AW-11129277199');`}
+                    </Script>
                     <link rel="icon" href="/images/favicon.ico"></link>
 										<meta
 											name="description"
