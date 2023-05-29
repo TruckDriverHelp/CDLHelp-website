1666039e89dc6ad4e321299fb526cb822300298e
alur2191 <dandavisjs@protonmail.com>
2023-05-29 15:07:06 -0700

    Meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -7,11 +7,28 @@ class MyDocument extends Document {
             <Html lang="eng">
                 <Head>
                     <link rel="icon" href="/images/favicon.ico"></link>
-                    <meta
-                        name="description"
-                        content="CDL Help это уникальное приложение с переводом CDL на русский в режиме реального времени. Лучший способ подготовки к сдачи тестов и обучению языка."
-                        key="desc"
-                    />
+                    {/* HTML Meta Tags */}
+                    <title>Приложение CDL Help - Тесты CDL на русском языке</title>
+                    <meta name="description" content="CDL Help - лучшее приложение с переводом CDL на русский в режиме реального времени. Лучший способ подготовки к сдачи тестов и обучению языка."/>
+
+                    {/* Google / Search Engine Tags */}
+                    <meta itemprop="name" content="Приложение CDL Help - Тесты CDL на русском языке"/>
+                    <meta itemprop="description" content="CDL Help - лучшее приложение с переводом CDL на русский в режиме реального времени. Лучший способ подготовки к сдачи тестов и обучению языка."/>
+                    <meta itemprop="image" content="https://cdlhelp.app/images/cdlhelp-tag.jpg"/>
+
+                    {/* Facebook Meta Tags */}
+                    <meta property="og:url" content="https://www.cdlhelp.app"/>
+                    <meta property="og:type" content="website"/>
+                    <meta property="og:title" content="Приложение CDL Help - Тесты CDL на русском языке"/>
+                    <meta property="og:description" content="CDL Help - лучшее приложение с переводом CDL на русский в режиме реального времени. Лучший способ подготовки к сдачи тестов и обучению языка."/>
+                    <meta property="og:image" content="https://cdlhelp.app/images/cdlhelp-tag.jpg"/>
+
+                    {/* Twitter Meta Tags */}
+                    <meta name="twitter:card" content="summary_large_image"/>
+                    <meta name="twitter:title" content="Приложение CDL Help - Тесты CDL на русском языке"/>
+                    <meta name="twitter:description" content="CDL Help - лучшее приложение с переводом CDL на русский в режиме реального времени. Лучший способ подготовки к сдачи тестов и обучению языка."/>
+                    <meta name="twitter:image" content="https://cdlhelp.app/images/cdlhelp-tag.jpg"/>
+
                 </Head>
                 <body>
                     <Main />
