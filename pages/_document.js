c5af95e769b4cdc8adf34deda4e599ae9f844364
alur2191 <dandavisjs@protonmail.com>
2023-09-22 16:57:01 -0700

    Integrated facebook pixel

--- pages/_document.js
+++ pages/_document.js
@@ -27,24 +27,7 @@ class MyDocument extends Document {
                     <meta name="twitter:title" content="Приложение CDL Help - Тесты CDL на русском языке"/>
                     <meta name="twitter:description" content="CDL Help - лучшее приложение с переводом CDL на русский в режиме реального времени. Лучший способ подготовки к сдачи тестов и обучению языка."/>
                     <meta name="twitter:image" content="https://cdlhelp.app/images/cdlhelp-tag.jpg"/>
-                    <script dangerouslySetInnerHTML={{
-                        __html: `
-                            !function(f,b,e,v,n,t,s)
-                            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                            n.queue=[];t=b.createElement(e);t.async=!0;
-                            t.src=v;s=b.getElementsByTagName(e)[0];
-                            s.parentNode.insertBefore(t,s)}(window,document,'script',
-                            'https://connect.facebook.net/en_US/fbevents.js');
-                            fbq('init', '766792617985084'); 
-                            fbq('track', 'PageView');
-                        `
-                    }} />
-                    <noscript>
-                        <img height="1" width="1" style="display:none"
-                            src="https://www.facebook.com/tr?id=766792617985084&ev=PageView&noscript=1" />
-                    </noscript>
+
                 </Head>
                 <body>
                     <Main />
