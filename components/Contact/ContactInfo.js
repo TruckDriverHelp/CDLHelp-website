1aa088d51bbe943f98b89da17caaa2aa9a24f5c6
alur2191 <dandavisjs@protonmail.com>
2024-01-31 15:21:16 +0300

    + Transfer contact page from CDL School to CDL Help. + Change links to local page, not school.cdlhelp
    Co-authored-by: Al <alur2191@users.noreply.github.com>

--- components/Contact/ContactInfo.js
+++ components/Contact/ContactInfo.js
@@ -6,35 +6,33 @@ const ContactInfo = () => {
             <div className="contact-info-area pb-100">
                 <div className="container">
                     <div className="contact-info-inner">
-                        <h2>Have any question in mind please call or mail us</h2>
+                        <h2>Вы также можете воспользоваться следующими методами</h2>
 
                         <div className="row justify-content-center">
                             <div className="col-lg-4 col-md-6 col-sm-6">
                                 <div className="single-contact-info-box">
-                                    <div className="icon bg1">
-                                        <i className="ri-customer-service-2-line"></i>
+                                    <div className="icon">
+                                        <i className="ri-mail-line"></i>
                                     </div>
-                                    <h3><a href="tel:+321895-980008">(+321) 895-980 008</a></h3>
-                                    <h3><a href="tel:+321895-980008">(+321) 895-980 008</a></h3>
+                                    <h3><a href="mailto:contact@truckdriver.help">contact@truckdriver.help</a></h3>
                                 </div>
                             </div>
 
                             <div className="col-lg-4 col-md-6 col-sm-6">
                                 <div className="single-contact-info-box">
                                     <div className="icon">
-                                        <i className="ri-earth-line"></i>
+                                        <i className="ri-telegram-line"></i>
                                     </div>
-                                    <h3><a href="mailto:hello@texap.com">hello@texap.com</a></h3>
-                                    <h3><a href="mailto:info@texap.com">info@texap.com</a></h3>
+                                    <h3><a href="https://www.t.me/tdhsupport">Служба Поддержки</a></h3>
                                 </div>
                             </div>
 
                             <div className="col-lg-4 col-md-6 col-sm-6">
                                 <div className="single-contact-info-box">
-                                    <div className="icon bg2">
-                                        <i className="ri-map-pin-line"></i>
+                                    <div className="icon">
+                                        <i className="ri-telegram-line"></i>
                                     </div>
-                                    <h3>121 King St, Melbourne VIC 3000, Australia.</h3>
+                                    <h3><a href="https://www.t.me/truckdrivergroup">Группа Телеграм</a></h3>
                                 </div>
                             </div>
                         </div>
