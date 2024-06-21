8493ea3f48fc29bebd0c701d03a168567d16deb2
om1ji <om1ji.music@gmail.com>
2024-06-21 13:58:01 +0300

    implement meta pixel

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script'
+import FACEBOOK_PIXEL_1 from "@/components/Pixel/facebook/pixel-1"
 
 class MyDocument extends Document {
     render() {
@@ -28,6 +28,7 @@ class MyDocument extends Document {
                     <meta name="twitter:description" content="CDL Help - лучшее приложение с переводом CDL на русский в режиме реального времени. Лучший способ подготовки к сдачи тестов и обучению языка."/>
                     <meta name="twitter:image" content="https://cdlhelp.com/images/cdlhelp-tag.jpg"/>
 
+                    <FACEBOOK_PIXEL_1 />
                 </Head>
                 <body>
                     <Main />
