9230755439e916f91a473b74356d64e9b6549d99
Al <dandavisjs@protonmail.com>
2024-07-07 00:49:37 -0400

    favicon

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,17 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script'
 
 class MyDocument extends Document {
     render() {
         return (
             <Html lang="ru">
                 <Head>
-                    <link rel="icon" href="/images/favicon.ico"></link>
+                    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                    <link rel="manifest" href="/site.webmanifest"/>
+                    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+                    <meta name="msapplication-TileColor" content="#da532c"/>
+                    <meta name="theme-color" content="#ffffff"/>
                     {/* HTML Meta Tags */}
                     <meta name="description" content="CDL Help - лучшее приложение с переводом CDL на русский в режиме реального времени. Лучший способ подготовки к сдачи тестов и обучению языка."/>
 
