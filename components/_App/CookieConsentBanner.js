9f53d982e9f73626c21d6b173024c9e2fb7d959e
alur2191 <dandavisjs@protonmail.com>
2024-09-19 23:01:55 +0300

    layout: cookie banner static

--- components/_App/CookieConsentBanner.js
+++ components/_App/CookieConsentBanner.js
@@ -2,23 +2,39 @@ import React, { useEffect, useState } from "react";
 import cookie from "js-cookie";
 import { useRouter } from "next/router";
 
+const localeContent = {
+  "ru": {
+    text: "Мы используем файлы cookie для улучшения вашего опыта на нашем сайте. Продолжая использовать наш сайт, вы соглашаетесь с использованием файлов cookie",
+    accept: "Принять",
+    deny: "Отклонить"
+  },
+
+  "en": {
+    text: "We use cookies to enhance your experience on our website. By continuing to use our site, you agree to our use of cookies",
+    accept: "Accept",
+    deny: "Deny"
+  },
+
+  "uk": {
+    text: "Ми використовуємо файли cookie для покращення вашого досвіду на нашому вебсайті. Продовжуючи використовувати наш сайт, ви погоджуєтеся з використанням файлів cookie",
+    accept: "Прийняти",
+    deny: "Відхилити"
+  }
+}
+
 const CookieConsentBanner = () => {
   const [showBanner, setShowBanner] = useState(false);
   const [texts, setTexts] = useState(null);
   const {locale} = useRouter();
 
+  const {text, accept, deny} = localeContent[locale];
+
   useEffect(() => {
     const consentCookie = cookie.get("cookieConsent");
-    fetch(`http://146.190.47.164:1337/api/cookie-banner?locale=${locale}`)
-    .then(response => response.json())
-    .then(data => {
-        setTexts(data.data.attributes);
-    })
-    .catch(error => {
-        console.error('Error fetching text data:', error);
-    })
 
-    if (!consentCookie) {
+    setTexts()
+
+    if (consentCookie) {
       setShowBanner(true);
     }
 }, []);
@@ -33,7 +49,7 @@ const CookieConsentBanner = () => {
     cookie.set("cookieConsent", "rejected", { expires: 365 });
   };
 
-  if (!showBanner || !texts) {
+  if (!showBanner) {
     return null;
   }
 
@@ -43,10 +59,9 @@ const CookieConsentBanner = () => {
       right: "10%", // sorry
       position: "fixed",
       zIndex: "99",
-      padding: "30px 50px",
+      padding: "10px 10px",
       width: "80%",
       margin: "0 auto",
-      textAlign: "center",
       borderTop: "2px solid #75759E",
       borderRight: "2px solid #75759E",
       borderLeft: "2px solid #75759E",
@@ -55,14 +70,14 @@ const CookieConsentBanner = () => {
       alignItems: "center",
       justifyContent: "center"
     }}>
-       <p style={{color: "#000000"}}>{texts.Text}</p>
+       <p style={{color: "#000000"}}>{text}</p>
        
         <div style={{
           display: "flex",
           justifyContent: "space-between",
         }}>
-          <button onClick={handleAccept} className="default-btn" style={{marginRight: "20px"}}>{texts.Accept}</button>
-          <button onClick={handleReject} className="default-btn">{texts.Decline}</button>
+          <button onClick={handleAccept} className="default-btn" style={{marginRight: "20px"}}>{accept}</button>
+          <button onClick={handleReject} className="default-btn">{deny}</button>
         </div>
     </div>
   );
