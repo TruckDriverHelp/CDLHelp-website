d866d98c4024dd414af9871641ca9f5cda844138
alur2191 <dandavisjs@protonmail.com>
2024-11-07 01:30:48 +0300

    layout: some fixes and remove meta

--- components/Article/texasCVOcard.js
+++ components/Article/texasCVOcard.js
@@ -2,18 +2,21 @@ import React from 'react';
 
 const CVOCard = ({ locale, questionEn, answerEn, questionLang, answerLang }) => {
   return (
-    <div className="tx-quiz-card">
+    <div className="tx-quiz-card" style={{ gridTemplateColumns: locale == "en" ? "auto" : "1fr 30px 1fr" }}>
       <div className="tx-quiz-card-column">
         <div>{questionEn}</div>
         <div>{answerEn}</div>
       </div>
-      <div className="tx-card-line"></div>
 
       {!(locale == "en") &&
+        <>
+          <div className="tx-card-line"></div>
+
           <div className="tx-quiz-card-column">
             <div>{questionLang}</div>
             <div>{answerLang}</div>
           </div>
+        </>
       }
     </div>
   );
