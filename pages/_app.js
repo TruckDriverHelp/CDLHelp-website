18acca970820491924b4a75a00fda9a5a5eca439
Al <dandavisjs@protonmail.com>
2025-05-24 19:13:49 -0700

    Added support button with Tawk.to

--- pages/_app.js
+++ pages/_app.js
@@ -67,6 +67,24 @@ const MyApp = ({ Component, pageProps, articles }) => {
                 `,
 					}}
 				/>
+				<Script
+					id="tawkto"
+					strategy="lazyOnload"
+					dangerouslySetInnerHTML={{
+						__html: `
+                  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+                  (function(){
+                    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                    s1.async=true;
+                    s1.src='https://embed.tawk.to/68327c58c87457190bdbd5f1/1is2jbec6';
+                    s1.charset='UTF-8';
+                    s1.setAttribute('crossorigin','*');
+                    s0.parentNode.insertBefore(s1,s0);
+                  })();
+                `,
+					}}
+				/>
+				
 			</Layout>
 		</QuizContextProvider>
 	)
