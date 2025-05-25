ee4885de8f8d3e0fb3489aec03d3d54c43532f99
alur2191 <dandavisjs@protonmail.com>
2025-05-24 19:22:33 -0700

    Changed Tawk.to ID

--- pages/_app.js
+++ pages/_app.js
@@ -72,16 +72,16 @@ const MyApp = ({ Component, pageProps, articles }) => {
 					strategy="lazyOnload"
 					dangerouslySetInnerHTML={{
 						__html: `
-                  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-                  (function(){
-                    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-                    s1.async=true;
-                    s1.src='https://embed.tawk.to/68327c58c87457190bdbd5f1/1is2jbec6';
-                    s1.charset='UTF-8';
-                    s1.setAttribute('crossorigin','*');
-                    s0.parentNode.insertBefore(s1,s0);
-                  })();
-                `,
+						var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+						(function(){
+						var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+						s1.async=true;
+						s1.src='https://embed.tawk.to/670840f72480f5b4f58b9589/1i9s3rhun';
+						s1.charset='UTF-8';
+						s1.setAttribute('crossorigin','*');
+						s0.parentNode.insertBefore(s1,s0);
+						})();
+					`,
 					}}
 				/>
 				
