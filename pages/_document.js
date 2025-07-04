13418092339cb9892e99528d0ce8ec8e2ac221a7
alur2191 <dandavisjs@protonmail.com>
2025-07-04 01:29:29 -0400

    Remove Tawk.to and implement Google Tag Manager

--- pages/_document.js
+++ pages/_document.js
@@ -25,6 +25,17 @@ class MyDocument extends Document {
                     
                 </Head>
                 <body>
+                    {/* Google Tag Manager (noscript) */}
+                    {process.env.NEXT_PUBLIC_GTM_ID && (
+                        <noscript>
+                            <iframe 
+                                src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+                                height="0" 
+                                width="0" 
+                                style={{ display: 'none', visibility: 'hidden' }}
+                            />
+                        </noscript>
+                    )}
                     <Main />
                     <NextScript />
                 </body>
