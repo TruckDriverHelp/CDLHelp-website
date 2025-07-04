d88890cee21f66d4b585e5e0cc54d4b7689230d6
Al <dandavisjs@protonmail.com>
2025-07-04 18:07:31 -0400

    Fixed the PostCSS configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   plugins: [
-    require('postcss-flexbugs-fixes'),
+    'postcss-flexbugs-fixes',
     [
-      require('postcss-preset-env'),
+      'postcss-preset-env',
       {
         autoprefixer: {
           flexbox: 'no-2009',
@@ -13,60 +13,5 @@ module.exports = {
         },
       },
     ],
-    // PurgeCSS configuration for production builds
-    ...(process.env.NODE_ENV === 'production'
-      ? [
-          [
-            require('@fullhuman/postcss-purgecss'),
-            {
-              content: [
-                './pages/**/*.{js,jsx,ts,tsx}',
-                './components/**/*.{js,jsx,ts,tsx}',
-                './src/**/*.{js,jsx,ts,tsx}',
-                './lib/**/*.{js,jsx,ts,tsx}',
-              ],
-              defaultExtractor: content => {
-                // Capture as liberally as possible, including things like `h-(screen-1.5)`
-                const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
-
-                // Capture classes within other delimiters like .block(class="w-1/2") in Pug
-                const innerMatches =
-                  content.match(/[^<>"'`\s.(){}[\]#=%]*[^<>"'`\s.(){}[\]#=%:]/g) || [];
-
-                return broadMatches.concat(innerMatches);
-              },
-              safelist: {
-                standard: [
-                  /^(fade|show|collapse|collapsing)/,
-                  /^modal/,
-                  /^tooltip/,
-                  /^popover/,
-                  /^bs-/,
-                  /^carousel/,
-                  /^dropdown/,
-                  /^nav/,
-                  /^navbar/,
-                  /^alert/,
-                  /^animated/,
-                  /^accordion/,
-                  /^swiper/,
-                  /^react-tabs/,
-                  /^yarl/,
-                  /^direction-rtl/,
-                  /^direction-ltr/,
-                ],
-                deep: [/^modal/, /^tooltip/, /^popover/, /^dropdown/],
-                greedy: [/active/, /show/, /open/, /collapsed/],
-              },
-              // Don't remove keyframe animations
-              keyframes: true,
-              // Font-face rules
-              fontFace: true,
-              // Variables
-              variables: true,
-            },
-          ],
-        ]
-      : []),
   ],
 };
